363bfce4d94333201939f38a8be0244a9fa8ace8
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:30:46 +0300

    up packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import { initThemeMode } from "flowbite-react/theme/mode-script";
 import { createRoot } from "react-dom/client";
+import { ThemeInit } from "../.flowbite-react/init";
 import { Main } from "./main";
 import "./index.css";
 
 const container = document.querySelector("#root") as Element;
 const root = createRoot(container);
 
-root.render(<Main />);
+root.render(
+  <>
+    <ThemeInit />
+    <Main />
+  </>,
+);
 
 initThemeMode();
